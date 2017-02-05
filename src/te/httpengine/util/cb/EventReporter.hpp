e8ba3b5dd9a630e3f8536a859caa46a341bb5cc3
Jesse Nicholson <ascensionsystems@gmail.com>
2017-02-05 14:35:49 -0500

    License change
    Change license to MPL 2.0.

--- src/te/httpengine/util/cb/EventReporter.hpp
+++ src/te/httpengine/util/cb/EventReporter.hpp
@@ -1,33 +1,10 @@
 /*
-* Copyright (c) 2016 Jesse Nicholson.
-*
-* This file is part of Http Filtering Engine.
-*
-* Http Filtering Engine is free software: you can redistribute it and/or
-* modify it under the terms of the GNU General Public License as published
-* by the Free Software Foundation, either version 3 of the License, or (at
-* your option) any later version.
-*
-* In addition, as a special exception, the copyright holders give
-* permission to link the code of portions of this program with the OpenSSL
-* library.
-*
-* You must obey the GNU General Public License in all respects for all of
-* the code used other than OpenSSL. If you modify file(s) with this
-* exception, you may extend this exception to your version of the file(s),
-* but you are not obligated to do so. If you do not wish to do so, delete
-* this exception statement from your version. If you delete this exception
-* statement from all source files in the program, then also delete it
-* here.
-*
-* Http Filtering Engine is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-* Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with Http Filtering Engine. If not, see <http://www.gnu.org/licenses/>.
+* Copyright © 2017 Jesse Nicholson
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
+
 
 #pragma once
 
